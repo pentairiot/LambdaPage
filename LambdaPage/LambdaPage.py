afa42a3f06b44a3f99dbbc6dcd92dc623901a3b8
Finn Blackwood <finn.blackwood@pentair.com>
2019-03-06 16:37:21 -0500

    Add text/html content-type exclusion to response encoding

--- LambdaPage/LambdaPage.py
+++ LambdaPage/LambdaPage.py
@@ -41,7 +41,7 @@ class LambdaPage:
             body = resp
         if not isinstance(body, str):
             body = json.dumps(body)
-        if not func.content_type == 'application/json':
+        if func.content_type not in ['application/json', 'text/html']:
             body = body.encode()
         return {"statusCode": status_code,
                 "headers": {"content-type": func.content_type},
